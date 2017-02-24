c71b81e5d5ba7bb68c0fa8c17bb56a59e3539c3d
rarnu <hexiaojie@hujiang.com>
2017-02-24 17:19:23 +0800

    * add build script for mac

---
+++ build_mac.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+TYPHON_ROOT=/usr/local/codetyphon
+FPC=${TYPHON_ROOT}/fpc/fpc32/bin/i386-darwin/fpc
+TYPHON=${TYPHON_ROOT}/typhon
+FW=/System/Library/Frameworks
+ARCH=i386-darwin
+
+# clean
+rm -fr smaliide.app
+
+# compile
+${FPC} -MObjFPC -Scghi -O1 -gw -gl -l -vewnhibq  \
+	-k-framework -kCarbon -k-framework -kOpenGL \
+	-k-dylib_file -k${FW}/OpenGL.framework/Versions/A/Libraries/libGL.dylib:${FW}/OpenGL.framework/Versions/A/Libraries/libGL.dylib \
+	-Fibuild/${ARCH} \
+	-FUbuild/${ARCH} \
+	-Fu . -Fuform -Fubase -Fuutil -Fuview -Fuview/synedit -Fuapi \
+	-Fu${TYPHON}/lcl/units/${ARCH}/carbon \
+	-Fu${TYPHON}/lcl/units/${ARCH} \
+	-Fu${TYPHON}/components/BaseUtils/lib/${ARCH} \
+	-Fu${TYPHON}/packager/units/${ARCH} \
+	-dLCL -dLCLcarbon \
+	smaliide.lpr
+
+# bundle
+mkdir smaliide.app
+mkdir smaliide.app/Contents
+mkdir smaliide.app/Contents/MacOS
+mkdir smaliide.app/Contents/Resources
+cp PkgInfo smaliide.app/Contents/PkgInfo
+cp Info.plist smaliide.app/Contents/Info.plist
+mv smaliide smaliide.app/Contents/MacOS/
+cp -r bin smaliide.app/Contents/MacOS/
+cp -r lib smaliide.app/Contents/MacOS/
+cp -r style  smaliide.app/Contents/MacOS/
+cp -r template smaliide.app/Contents/MacOS/
+cp -r image smaliide.app/Contents/MacOS/
+
+
