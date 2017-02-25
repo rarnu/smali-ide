f69d00de5c867ccfa3048364fd76326a6c333e78
rarnu <hexiaojie@hujiang.com>
2017-02-25 15:38:42 +0800

    * add build script for windows

---
+++ build_windows.bat
@@ -0,0 +1,16 @@
+SET TYPHON_ROOT=C:\codetyphon
+SET TYPHON=%TYPHON_ROOT%\typhon
+SET FPC=%TYPHON_ROOT%\fpc\fpc64\bin\x86_64-win64\fpc.exe
+SET ARCH=x86_64-win64
+
+%FPC% -MObjFPC -Scghi -O1 -g -gl -WG -l -vewnhibq ^
+	-Fibuild\%ARCH% ^
+	-FUbuild\%ARCH% ^
+	-Fu. -Fuform -Fubase -Fuutil -Fuview -Fuview\synedit -Fuapi ^
+	-Fu%TYPHON%\lcl\units\%ARCH%\win32 ^
+	-Fu%TYPHON%\lcl\units\%ARCH% ^
+	-Fu%TYPHON%\components\BaseUtils\lib\%ARCH% ^
+	-Fu%TYPHON%\packager\units\%ARCH% ^
+	-dLCL -dLCLwin32 ^
+	smaliide.lpr
+
